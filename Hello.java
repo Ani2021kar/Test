4aa379f0dbd4d9f01b11e8b32c668bc2754d33c6
Aniket Sonkar <aniketsonkar721301@gmail.com>
2022-03-26 12:23:03 +0530

    Fanta Coke

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@ public class Hello{
     public static void main(String[] args) {
         System.out.println("Hey! I am fine");
         System.out.println("Hey! I am Good");
+        System.out.println("Fanta");
+        System.out.println("Coke");
     }
 }
