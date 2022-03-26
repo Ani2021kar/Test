262bcd5333b3190c7297d8fba1aef9677c32baa9
Aniket Sonkar <aniketsonkar721301@gmail.com>
2022-03-26 12:46:40 +0530

    MERGED

--- Hello.java
+++ Hello.java
@@ -2,15 +2,15 @@ public class Hello{
     public static void main(String[] args) {
         System.out.println("Hey! I am fine");
         System.out.println("Hey! I am Good");
-<<<<<<< HEAD
+
         System.out.println("Fanta");
         System.out.println("Coke");
-=======
+
 
         System.out.println("I am Thirsty");
         System.out.println("I want to drink water");
         System.out.println("I want to Drink Lemonade");
         
->>>>>>> thirsty
+
     }
 }
