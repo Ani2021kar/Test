902570816e3afe1f55d90de46ec8404b21d81f3b
Aniket Sonkar <aniketsonkar721301@gmail.com>
2022-03-26 12:03:34 +0530

    Water thirst

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@ public class Hello{
     public static void main(String[] args) {
         System.out.println("Hey! I am fine");
         System.out.println("Hey! I am Good");
+
+        System.out.println("I am Thirsty");
     }
 }
